c0b319ccca224f5cad5b39f8c466d641ebdf7424
modernToking_ <33455244+GalymzhanAbdimanap@users.noreply.github.com>
2021-02-24 16:02:30 +0600

    Update Individ.py

--- genetic_alghoritm_v4_1/Individ.py
+++ genetic_alghoritm_v4_1/Individ.py
@@ -7,6 +7,6 @@ class Individ:
     
     def __init__(self, y1, y2, length):   
         self.length = length   
-        self.A=np.random.randint(y1, y2, self.length)    #Объявляем атрибут "А" нашего индивида (это будет сама строка 010..101)
-        self.fit=0                          #Объявляем атрибут "живучести" индивида (это будет сумма элементов нашей строки 010..101) (пока присвоим ей значение 0)
+        self.A=np.random.randint(y1, y2, self.length)    # Declare the attribute "A" of our individ (this will be the line 010..101).
+        self.fit=0                          # Declare the attribute "fit" of the individ (this will be the sum of the elements of our line 010..101) (for now, we will assign it the value 0)
     
