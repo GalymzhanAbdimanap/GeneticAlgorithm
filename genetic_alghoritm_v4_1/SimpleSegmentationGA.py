0b33e054ca163998920ced4ccc664702d27e8561
modernToking_ <33455244+GalymzhanAbdimanap@users.noreply.github.com>
2021-02-24 16:54:05 +0600

    Update SimpleSegmentationGA.py

--- genetic_alghoritm_v4_1/SimpleSegmentationGA.py
+++ genetic_alghoritm_v4_1/SimpleSegmentationGA.py
@@ -14,7 +14,7 @@ class SimpleSegmentationGA(GeneticAlghoritm):
     def __init__(self, pop_num, lenght, delta_x, gray):
         self.pop_num = pop_num
         self.lenght = lenght    # Length of individ.
-        self.delta_x =  delta_x # value of h
+        self.delta_x =  delta_x # Value of h.
         self.gray = 256 - gray
         
 
@@ -24,41 +24,41 @@ class SimpleSegmentationGA(GeneticAlghoritm):
 
         self.y1 = y1
         self.y2 = y2
-        self.population = []                    # Declare an array "population" in which we will store a population of 5 individuals.
+        self.population = []                                               # Declare an array "population" in which we will store a population of 5 individuals.
         for i in range(self.pop_num):           
-            c=Individ(self.y1, self.y2, self.lenght)                # Create a new individual.
-            self.population.append(c)           # Add the i-th unique individual to the array (fill in our population)
+            c=Individ(self.y1, self.y2, self.lenght)                       # Create a new individual.
+            self.population.append(c)                                      # Add the i-th unique individual to the array (fill in our population)
                                   
         
-        self.mother = Individ(self.y1, self.y2, self.lenght)                     # Initialize the variables with which we will work: mom, dad, 4 sons ..
+        self.mother = Individ(self.y1, self.y2, self.lenght)               # Initialize the variables with which we will work: mom, dad, 4 sons ..
         self.father = Individ(self.y1, self.y2, self.lenght)                     
         self.son1 = Individ(self.y1, self.y2, self.lenght)                       
         self.son2 = Individ(self.y1, self.y2, self.lenght)
         self.son3 = Individ(self.y1, self.y2, self.lenght)
         self.son4 = Individ(self.y1, self.y2, self.lenght)
-        self.par_and_sons = []                                                   #.. and an array of individs "Parents and Children" in which we will store
+        self.par_and_sons = []                                             #.. and an array of individs "Parents and Children" in which we will store
         
-        for j in range(self.pop_num*3):                    # Initialize our array of "Parents and Sons" with random individs.
+        for j in range(self.pop_num*3):                                    # Initialize our array of "Parents and Sons" with random individs.
             self.par_and_sons.append(Individ(self.y1, self.y2, self.lenght))       
         
 
     def cross(self):
         """Crosses the best individs with each other."""
 
-        for i in range(self.pop_num):                # Put in the first pop_num elements of the "Parents and Sons" array our entire input population.
+        for i in range(self.pop_num):                                      # Put in the first pop_num elements of the "Parents and Sons" array our entire input population.
             self.par_and_sons[i].A=self.population[i].A.copy()
 
-        random.shuffle(self.population) # 
+        random.shuffle(self.population)                                    # Shuffle population.
 
-        tt=0                                       # The counter that is needed to implement a non-trivial crossing.
-        for s in range(0,self.pop_num,2):                    # From 0 to pop_num with step 2. That is. here we take pop_num / 2 pairs of parents.
-            self.mother.A=self.population[tt+int(self.pop_num/2)].A      # Let the last pop_num / 2 individuals of our population be our mothers.
-            self.father.A=self.population[tt].A                           # And let first pop_num / 2 individuals of our population be dads.
+        tt=0                                                               # The counter that is needed to implement a non-trivial crossing.
+        for s in range(0,self.pop_num,2):                                  # From 0 to pop_num with step 2. That is. here we take pop_num / 2 pairs of parents.
+            self.mother.A=self.population[tt+int(self.pop_num/2)].A        # Let the last pop_num / 2 individuals of our population be our mothers.
+            self.father.A=self.population[tt].A                            # And let first pop_num / 2 individuals of our population be dads.
         
             tt=tt+1    
             ran=random.random()
 
-            for n in range(self.lenght):              # Crossover.
+            for n in range(self.lenght):                                   # Crossover.
                 if random.random()>0.5:
                     self.son1.A[n] = self.father.A[n]
                     self.son2.A[self.lenght-1-n] = self.father.A[n]
@@ -79,7 +79,7 @@ class SimpleSegmentationGA(GeneticAlghoritm):
     def mutation(self):
         """Mutates individuals with a 20% probability."""
 
-        for r in range(self.pop_num*3, 5):                # Mutation.
+        for r in range(self.pop_num*3, 5):                                 # Mutation.
             for w in range(0,self.lenght):              
                 if random.random()<0.2:                  
                     self.par_and_sons[r].A[w] = self.par_and_sons[r].A[w] + np.random.randint(-20, 20)  # Offset + -20 pixels.
@@ -89,7 +89,7 @@ class SimpleSegmentationGA(GeneticAlghoritm):
     def selection(self):
         """Sorts by fit and selects the best pop_num individs."""
 
-        for i in range(self.pop_num*3):                     # It is important. Next, we will rank the array of parents and children in ascending order of survivability (sum (fit)).
+        for i in range(self.pop_num*3):                                    # It is important. Next, we will rank the array of parents and children in ascending order of survivability (sum (fit)).
             self.par_and_sons[i].fit = fitness(self.gray, self.delta_x, self.lenght, self.par_and_sons[i].A)
 
         #  Sort.
