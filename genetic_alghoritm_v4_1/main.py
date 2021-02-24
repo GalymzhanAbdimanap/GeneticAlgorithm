c632b36dbe3439f1c550efbeb007701f3e78e43e
modernToking_ <33455244+GalymzhanAbdimanap@users.noreply.github.com>
2021-02-24 16:50:27 +0600

    Update main.py

--- genetic_alghoritm_v4_1/main.py
+++ genetic_alghoritm_v4_1/main.py
@@ -11,13 +11,24 @@ if __name__ == '__main__':
     EPOCH=100
     IMAGE_PATH = '7.jpg'
 
+    # Image operations. 
     image = cv2.imread(IMAGE_PATH)
     height, width = image.shape[:2]
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Calculate individ's lenght.
+    length_individ = int(width/DELTAX)
+
+    # Calculate the range of random numbers for an individ.
     peaks = find_peaks_(gray) #[:2]
-    length_ = int(width/DELTAX)
-    ssga = SimpleSegmentationGA(POP_NUM, length_, DELTAX, EPOCH, gray)
+    
+    # Create object of class.
+    ssga = SimpleSegmentationGA(POP_NUM, length_individ, DELTAX, gray)
+
     start_time = time.time()
+    # Run genetic algorithm.
     lines = run(ssga, peaks, EPOCH)
     print('Time run=', time.time()-start_time)
+
+    # Draw the result of the genetic alghoritm.
     drawLines(image, lines[-1], DELTAX)
