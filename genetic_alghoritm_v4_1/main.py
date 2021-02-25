5d7868aee4e33a450d95ab479e2f3ad972ace69f
modernToking_ <33455244+GalymzhanAbdimanap@users.noreply.github.com>
2021-02-25 19:00:47 +0600

    Update main.py

--- genetic_alghoritm_v4_1/main.py
+++ genetic_alghoritm_v4_1/main.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
 
     images = glob.glob(f'{IMAGES_PATH}/*.jpg')
     for imagename in images:
-
+        fstart_time = time.time()
         # Image operations. 
         image = cv2.imread(imagename)
         height, width = image.shape[:2]
@@ -34,8 +34,17 @@ if __name__ == '__main__':
 
         start_time = time.time()
         # Run genetic algorithm.
-        lines = run(ssga, peaks, EPOCH)
-        print('Time run=', time.time()-start_time)
+        lines = run(ssga, peaks, EPOCH, parallel=False)
+        print('Time run =', time.time()-start_time)
 
+        start_time = time.time()
         # Draw the result of the genetic alghoritm. lines[-1] -> result of last epoch.
-        drawLines(image, lines[-1], DELTAX, os.path.basename(imagename))
+        draw_lines(image, lines[-1], DELTAX, os.path.basename(imagename))
+        print('Time draw_lines =', time.time()-start_time)
+
+        start_time = time.time()
+        # Crop the result of genetic alghoritm.
+        crop_lines(image, lines[-1], DELTAX, os.path.basename(imagename))
+        print('Time crop_lines =', time.time()-start_time)
+        print('Time for one image =', time.time()-fstart_time)
+        
